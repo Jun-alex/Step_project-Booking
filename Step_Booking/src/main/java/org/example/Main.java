27f42bcd07f1b279f2ea759266fad78d0ba9e42c
Vladyslav Soruk <sorukv@gmail.com>
2023-11-07 14:30:43 +0200

    displayFlightsWithin24Hours method done

--- Step_Booking/src/main/java/org/example/Main.java
+++ Step_Booking/src/main/java/org/example/Main.java
@@ -1,23 +1,25 @@
 package org.example;
 
 import org.example.flightDao.CollectionFlightDao;
+import org.example.flightDao.FlightController;
+import org.example.flightDao.FlightService;
 import org.example.model.Flight;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+//        При створенні конструктора collectionFlightDao ми генеруємо 501 рейс
+//        (з айді від 0 до 500 включно), завантажуємо їх у файл flights.json
+//        та зберігаємо їх в List<Flight> flightsDatabase
         CollectionFlightDao collectionFlightDao = new CollectionFlightDao();
-        collectionFlightDao.getFlightsDatabase().forEach(System.out::println);
+        FlightService flightService = new FlightService(collectionFlightDao);
+        FlightController flightController = new FlightController(flightService);
 
-        Flight flightById = collectionFlightDao.getFlightById(1);
-        System.out.println(flightById);
-        Flight flightById2 = collectionFlightDao.getFlightById(0);
-        System.out.println(flightById2);
-        Flight flightById3 = collectionFlightDao.getFlightById(50);
-        System.out.println(flightById3);
-
-        List<Flight> vienna = collectionFlightDao.getFlightByUserInfo("Vienna", "10.11.2023", 10);
-        System.out.println(vienna);
+        flightController.displayFlightsWithin24Hours();
+        System.out.println(flightController.getFlightById(0));
+        System.out.println(flightController.getFlightById(1));
+        System.out.println(flightController.getFlightByUserInfo("Madrid", "11.11.2023", 10));
+//        flightController.getFlightsDatabase().forEach(System.out::println);
     }
 }
